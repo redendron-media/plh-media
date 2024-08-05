752244e1dd8a1dc37aa904f530f71080e0723cad
redendron-media <122364900+redendron-media@users.noreply.github.com>
2024-08-05 09:40:22 +0530

    LeadQuizzes embed code

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,16 +4,22 @@ import Script from 'next/script';
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+export const metadata   
+ = {
   title: "PLH Media",
   description: "Helping founders get more eyeballs on their brand and more money in their business",
 };
 
 export default function RootLayout({ children }) {
+  const iframeUrl = 'https://content.leadquizzes.com/lp/FRAGi-3b_w?embed=1'; // Replace with your actual iframe URL
+
   return (
     <html lang="en">
       <head>
-      <Script src="https://lq3-production01.s3.amazonaws.com/lead_quizzes_3.0/tracking/js/properties/ihexyypuapuk3g.js"/>
+        <Script src="https://lq3-production01.s3.amazonaws.com/lead_quizzes_3.0/tracking/js/properties/ihexyypuapuk3g.js" />
+        <div id='inline-embed-iframe' class='inline-embed-full' style={{ borderRadius: '5px', overflow: 'hidden' }}>
+          <iframe src={iframeUrl} frameborder="0" style={{ width: "100%", float: "left" }} />
+        </div>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
